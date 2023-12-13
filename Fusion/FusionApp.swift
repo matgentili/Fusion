134c899d01121e418378a9cf937e293af3e1a1f3
Matteo Gentili <matteogentili20@gmail.com>
2023-12-13 17:01:38 +0100

    First commit

---
+++ Fusion/FusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FusionApp.swift
+//  Fusion
+//
+//  Created by Matteo Gentili on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FusionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
